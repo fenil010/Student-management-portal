295b22d9a435d4f0498c03bd1bd86b085b8cc76e
Fenil Sakhiya <fp2367369@gmail.com>
2026-01-09 10:12:54 +0530

    bash changes

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 echo "📦 Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
 echo "📁 Collecting static files..."
 python manage.py collectstatic --no-input
